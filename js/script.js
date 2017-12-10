3b3721338078f14f5a388d86f3a55df992dd6939
ShesterovV <slava.shesterov@gmail.com>
2017-12-10 19:18:37 +0300

    Финишная прямая )

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
-  var link = document.querySelector(".letter");
-  var popup = document.querySelector(".write-us-form");
-  var close = popup.querySelector(".write-us-exit");
-  var login = popup.querySelector("[name=login]");
-  var email = popup.querySelector("[name=email]");
-  var storage = localStorage.getItem("login");
+var link = document.querySelector(".letter");
+var popup = document.querySelector(".write-us-form");
+var close = popup.querySelector(".write-us-exit");
+var login = popup.querySelector("[name=login]");
+var email = popup.querySelector("[name=email]");
+var storage = localStorage.getItem("login");
 
-  link.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.add("write-us-form-show");
-    if (storage) {
-      login.value = storage;
-      email.focus();
-    } else {
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("write-us-form-show");
+  if (storage) {
+    login.value = storage;
+    email.focus();
+  } else {
     login.focus();
-    }
+  }
 });
 
-  close.addEventListener("click", function (evt) {
+close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("write-us-form-show");
 });
@@ -25,15 +25,15 @@ window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     if (popup.classList.contains("write-us-form-show")) {
       popup.classList.remove("write-us-form-show");
-    }
+    } 
   }
 });
 
-  popup.addEventListener("submit", function (evt) {
-    if (!login.value || !email.value) {
-      evt.preventDefault();
-      console.log("Нужно заполнить все поля");
-    } else {
+popup.addEventListener("submit", function (evt) {
+  if (!login.value || !email.value) {
+    evt.preventDefault();
+    console.log("Нужно заполнить все поля");
+  } else {
     localStorage.setItem("login", login.value);
   }
 });
